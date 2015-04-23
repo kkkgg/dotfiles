101c0c8507108f7047261a976c2a216012267657
kkkgg <kkkgg@airwin>
2015-04-23 11:56:50 +0900

    Add cygwin initialize install shellscript

---
+++ initinstall.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+apt-cyg install curl
+apt-cyg install ssh
+apt-cyg install nc
+apt-cyg install screen
+apt-cyg install whois
+apt-cyg install libiconv
+apt-cyg install zip
+apt-cyg install unzip
+apt-cyg install vim
+apt-cyg install rdiff-backup
+apt-cyg install rsync
+apt-cyg install git
+apt-cyg install bc
+apt-cyg install chere
